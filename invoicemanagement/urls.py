d1f2df1b6f4834383597ad71fce4756bfa212bea
kamwanasamwel <kamwanasamwel@gmail.com>
2021-04-23 10:44:48 +0300

    before adding static

--- invoicemanagement/urls.py
+++ invoicemanagement/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from invoicemgmt import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('add_invoice/', views.add_invoice, name='add_invoice'),
+    path('list_invoice/', views.list_invoice, name='list_invoice'),
 ]
--- invoicemgmt/views.py
+++ invoicemgmt/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import InvoiceForm
+from .models import *
 # Create your views here.
+
+
+def home(request):
+    title = 'Welcome: This is the Home Page'
+    context = {
+        "title": title,
+    }
+    return render(request, "home.html", context)
+
+
+def add_invoice(request):
+    form = InvoiceForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/add_invoice')
+    context = {
+        "form": form,
+        "title": "New Invoice",
+    }
+    return render(request, "entry.html", context)
+
+
+def list_invoice(request):
+    title = 'List of Invoices'
+    queryset = Invoice.objects.all()
+    context = {
+        "title": title,
+        "queryset": queryset,
+    }
+    return render(request, "list_invoice.html", context)
